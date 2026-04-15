5ddf6ca1d003c6e6f2a9d5f805a61a756368e693
Santosh <krishna.@Krishnas-MacBook-Air.local>
2026-04-14 21:10:28 -0700

    Add NotificationDrawer floating handle to home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -517,6 +517,7 @@ export default function Dashboard() {
         ::-webkit-scrollbar { display: none; }
       `}</style>
 
+      <NotificationDrawer unreadCount={3}/>
       <BottomNav />
     </div>
   )
